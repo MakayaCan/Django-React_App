948b083d7c65a294bbcce90e3c1d676f43d6425d
Canisio Makaya <makayac@telecelzw.co.zw>
2025-08-15 15:04:47 +0200

    Updating RESTful APIs on tut 2

--- api/views.py
+++ api/views.py
@@ -4,7 +4,8 @@ from .serializers import RoomSerializer
 from .models import Room
 
 # Create your views here.
-class RoomView(generics.CreateAPIView):
+class RoomView(generics.ListAPIView):
+#class RoomView(generics.CreateAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
 
